165dd9d530fecc75bf3dbd65bd6f1b8ccaddfef6
Nathan Fikes <nathan@fikesfarm.com>
2023-11-21 22:42:58 -0500

    Cleaning up and Comments
    Documentation over the NANO program.

--- Vehicle_Programs/Arduino_Auto_Reaction/Arduino_Auto_Reaction.ino
+++ Vehicle_Programs/Arduino_Auto_Reaction/Arduino_Auto_Reaction.ino
@@ -1,6 +1,10 @@
-// 11/20/23 Emergency Automation superimposition on manual motor control.
+// Last Update 11/21/23 
+//
+// Emergency Automation superimposition on manual motor control.
 // Created by Nathan Fikes for use in controlling motors from various Ultrasound sensors.
 // Logic is unfinished. Thomas Tran
+//
+// Description: Program hosted on the Arduino NANO to handle all the data between the 4 ultrasound sensors, commands from the BLE peripheral, and handle motor control.
 
 #include <Servo.h>
 #include <math.h>    //round
@@ -10,23 +14,26 @@
 #define RATE_MICROS 16000
 #define SERIAL_REFRESH 32  //How many cycles to skip until reload SERIAL
 
+// If we need to show the sample rate for the ultrasounds, we can store the value here.
+static int sample_rate = 1 / (RATE_MICROS / 1000000);
+
 // Initialization of Ultrasound Variables
 
 // Threshold at which the Vehicle will produce certain actions. 5cm past lower bound.
-static int distanceThreshold = 7;  //cm
-int cm = 0;
-static double sample_rate = 40;  // 1/S how many samples per second.
+static int upperDistanceThreshold = 7;  // cm
+static int lowerDistanceThreshold = 3;  // cm
+int cm = 0;                             // Initialization of cm as a variable
 
-static int motor_steering_angle = 0;  // -90 to 90 steering angle
-static int motor_power = 0;           // -100% to 100% motor power
+static int motor_steering_angle = 0;    // -90 to 90 steering angle
+static int motor_power = 0;             // -100% to 100% motor power
 
-// For the driving motor.
-int PWM_pin_out = 11;
-int Dir_pin_out = 10;
+// For the driving motor. Pins used:
+int PWM_pin_out = 11;                   // Needs to be a ~ pin on the NANO
+int Dir_pin_out = 10;                   // Pin for pulling low/high for the nand gate h-bridge "fooling" circuit.
 
-// For the steering motor.
-int Steering_servo_pin_out = 9;  // The steering motor data line should be connected to D11
-int steer_max = 90;              // Max of 90 degree steer angle for now.
+// For the steering motor. Pin used:
+int Steering_servo_pin_out = 9;         // The steering motor data line should be connected to D11
+int steer_max = 90;                     // Max of 90 degree steer angle.
 
 Servo steering_servo;
 
@@ -42,42 +49,48 @@ long readUltrasonicDistance(int triggerPin, int echoPin) {
   digitalWrite(triggerPin, LOW);
 
   // Reads the echo pin, and returns the sound wave travel time in microseconds
+  // We need to use the RATE_MICROS here to make sure to ignore signals that are not useful.
   return pulseIn(echoPin, HIGH, RATE_MICROS / 2);
 }
 
+// Function to handle the driving motor. Takes a power from -100 to 100
 long motorPowerPWMandDir(int power) {
   // Function that should help set PWM pin without uneccesary math
   // Function should also set direction, aka H-Bridge control.
-  float pwm_decimal = abs(power) * 2.25;
+  float pwm_decimal = abs(power) * 2.25;          // Transforms power from 0 - +-100 to 0 - 225 all scaled in the positive dir
 
-  analogWrite(PWM_pin_out, round(pwm_decimal));
+  analogWrite(PWM_pin_out, round(pwm_decimal));   // PWM pin takes this number as an int.
 
   if (power >= 0) {
-    digitalWrite(Dir_pin_out, LOW);
+    digitalWrite(Dir_pin_out, LOW);               // If the power is greater than 0 we tell the H-Bridge to go forward.
   } else if (power < 0) {
-    digitalWrite(Dir_pin_out, HIGH);
+    digitalWrite(Dir_pin_out, HIGH);              // If the power is less than 0 we need to fool the H-Bridge to go backwards.
   } else {
-    Serial.print("Error in the motor direction, can't resolve direction. Check motorPowerPWMandDir function.");
+    Serial.print("Error in the motor direction, can't resolve direction. Check motorPowerPWMandDir function."); // I doubt this line can be reached.
   }
 }
 
+// Function to handle the steering servo. Takes an angle.
 long motorSteeringHandler(int steering_angle) {
   if (abs(steering_angle) >= steer_max) {
-    Serial.print("Error in motor steering, past motor max. Change steer_max global if more range is needed.");
+    Serial.print("Error in motor steering, past motor max.");
   } else {
     steering_servo.write(steering_angle + 90);
   }
 }
 
+// BAUD rate for serial communication using UART.
 void setupSerial() {
   Serial.begin(115200);
 }
 
+// For each sensor we need to set the pins accordingly, the echo pin must be an input and the trigger must be an output.
 void setupUltrasonicPins(int triggerPin, int echoPin) {
   pinMode(triggerPin, OUTPUT);  // Clear the trigger
   pinMode(echoPin, INPUT);
 }
 
+// Setup the ultrasonics using the pin configurations below.
 void setupUltrasonic() {
   //setupUltrasonicPins(1, 2);
   setupUltrasonicPins(3, 4);
@@ -96,12 +109,10 @@ void setup() {
   digitalWrite(Dir_pin_out, LOW);
 }
 
+// This is useful for reading serial commands.
 static String buffer = "";
 
 void loop() {
-  // Threshold at which the Vehicle will produce certain actions. 5cm past lower bound.
-  distanceThreshold = 7;  //cm
-
   // Ultrasound sensor used [Contactless Distance Measurement]: HC-SR04
   // Datasheet declared accuracy: 0.03 cm
   // Datasheet declared range: 2cm - 400cm
@@ -137,40 +148,39 @@ void loop() {
     */
   }
 
+  // We can read serial commands and interpret them to do certain tasks. In this case we will just consider motor functions. The lights will be handeled by the BLE server module. ESP32-WROVER-E.
   if (Serial.available()) {
-    char receivedChar = Serial.read();
+    char receivedChar = Serial.read();            // Grab a character from the serial communication RX line.
     if (receivedChar == '\n') {
-      Serial.print("Accumulated buffer: ");
-      Serial.println(buffer.c_str());
-
-      char message_type = buffer[0];
-      String message_value = buffer.substring(1);
+      Serial.print("Accumulated buffer: ");       // After awhile of accumulating things we will print it only if the recieved character is a special escape character \n.
+      Serial.println(buffer.c_str());             // Display this buffer string. This will be displayed on the OLED screen as a serial back out.
+      char message_type = buffer[0];              // The message type or tag is always the first character in the sequence.
+      String message_value = buffer.substring(1); // The value is everything afterwards. This is everything after the index 0 denoted as substring(1).
 
       switch (message_type) {
         case 'P':
           {
-            int motor_power = message_value.toInt();
+            int motor_power = message_value.toInt();          // Reads the motor power off this tag. "P"
             motorPowerPWMandDir(motor_power);
-
             break;
           }
         case 'S':
           {
-            int motor_steering_angle = message_value.toInt();
+            int motor_steering_angle = message_value.toInt(); // Reads the steering angle off this tag. "S"
             motorSteeringHandler(motor_steering_angle);
             break;
           }
         default:
           {
+            // Nothing in here.
           }
       }
-      buffer = "";  // clear buffer
+      buffer = "";              // clear buffer after reading the value and using it.
     } else {
-      // accumulate
-      buffer += receivedChar;
+      
+      buffer += receivedChar;   // Accumulate stuff into the buffer as long as the recieved char is not '\n'
     }
   }
-
 
   //   _______   _____ ______   _______   ________  ________  _______   ________   ________      ___    ___
   //  |\  ___ \ |\   _ \  _   \|\  ___ \ |\   __  \|\   ____\|\  ___ \ |\   ___  \|\   ____\    |\  \  /  /|
@@ -194,92 +204,5 @@ void loop() {
 
   // Protocols for each emercency case [What should the car do when something is too close?]:
 
-  /*
-  if (motor_power > 0){
-    // If we are driving, we can't assume any value for motor_power, be careful when assigning values to the motor functions.
-
-    if (UF_cm < distanceThreshold){
-      // We can't set this to 0, what should motor power be if there is something in front that is slower?
-      // Protip: scale motor power based on UF_cm reading.
-    } else if (UB_cm < distanceThreshold){
-      // We shouldn't accelerate crazy, what should motor power be if there is something behind moving faster?
-      // Protip: scale motor power based on BF_cm reading.
-    } else if (UB_cm < distanceThreshold && UF_cm < distanceThreshold){
-      // Uh oh, we are in a traffic jam. We don't want to speed up or slow down. In this case we want to maximize space.
-      // Protip: We should speed up if UF_cm < UB_cm or slow down if UF_cm > UB_cm. Scale these on U measurements.
-
-    } else if (UL_cm < distanceThreshold){
-      // Someone is too close to the left, which way should we turn?
-      // Protip: set steeringHandler() to a specific steering angle. Negative for left, positive for right.
-    } else if (UR_cm < distanceThreshold){
-      // Man now someone is on the right side, which way should we turn?
-      // Protip: set steeringHandler() to a specific steering angle. Negative for left, positive for right.
-    } else if (UL_cm < distanceThreshold && UR_cm < distanceThreshold){
-      // This is quite the pickle, we have two people on either side.
-      // Minimizing space would not be good due to sensitivity. If this occurs we should just drive straight.
-    }
-  } else if (motor_power == 0){
-    // If we are NOT driving, we are safe to assume motor_power is currently at 0
-
-    if (UF_cm < distanceThreshold){
-      // In this case someone is approaching from the front while we are stationary, where should we drive?
-      // Protip: scale motor power based on UF_cm reading.
-    } else if (UB_cm < distanceThreshold){
-      // Now someone is approaching the back, which direction should we drive?
-      // Protip: scale motor power based on BF_cm reading.
-    } else if (UB_cm < distanceThreshold && UF_cm < distanceThreshold){
-      // This is a real traffic jam where everyone is stopped. We should be careful where we drive.
-      // Protip: We should speed up if UF_cm < UB_cm or slow down if UF_cm > UB_cm. Scale these on U measurements.
-
-    } else if (UL_cm < distanceThreshold){
-      // Someone is too close to the left, which way should we turn? Also which direction do we drive to make space?
-      // Protip: This requires a special maneuver called a Jack-Knife generally used by Semi Trucks. The same concept applies here.
-      // Protip on Jack-Knife: To perform a Jack-Knife, turn toward the incoming obstacle, drive backwards.
-    } else if (UR_cm < distanceThreshold){
-      // Someone is too close to the right now, which way should we turn? Also which direction do we drive to make space?
-      // Protip: This requires a special maneuver called a Jack-Knife generally used by Semi Trucks. The same concept applies here.
-      // Protip on Jack-Knife: To perform a Jack-Knife, turn toward the incoming obstacle, drive backwards.
-    } else if (UL_cm < distanceThreshold && UR_cm < distanceThreshold){
-      // This is quite the pickle, we have two people on either side.
-      // Minimizing space would not be good due to sensitivity. If this occurs we should just brake [not move at all].
-    }
-  } else {
-    // If no emergency protocols are activated, we can just default the driving parameters into the motor functions.
-    //motorPowerPWMandDir(motor_power, motor_direction);
-    //motorSteeringHandler(motor_steering_angle);
-  }
-  */
-
-  /*
-  for (int i = 255; i >= 0; i--) {
-    analogWrite(PWM_pin_out, i);
-    delay(50);
-    digitalWrite(Dir_pin_out, HIGH);
-  }
-
-  for (int i = 0; i <= 255; i++) {
-    analogWrite(PWM_pin_out, i);
-    delay(50);
-    digitalWrite(Dir_pin_out, LOW);
-  }
-  */
-
-  /*
-  for (int i = -90; i <= 90; i++) {
-  motorSteeringHandler(i);
-  delay(10);
-  }
-  for (int i = 90; i >= -90; i--) {
-  motorSteeringHandler(i);
-  delay(10);
-  }
-  */
-  //motorPowerPWMandDir(-10);
-  //motorPowerPWMandDir(motor_power);
-  //motorSteeringHandler(motor_steering_angle);
-
-  //digitalWrite(PWM_pin_out, HIGH);
-
-  //delayMicroseconds(RATE_MICROS - micros()%RATE_MICROS);
-  //delay(round((1/sample_rate) * 1000));
+  delayMicroseconds(RATE_MICROS - micros()%RATE_MICROS);
 }
